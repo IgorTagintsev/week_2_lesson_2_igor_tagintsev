5df93d340c89aff23f7ce3a15e8c7bfd57d2b49a
Igor Tagintsev <t.ingvarr@gmail.com>
2018-10-06 21:37:08 +0300

    Fixed bag in task_6

--- utils/task_6.py
+++ utils/task_6.py
@@ -1,5 +1,4 @@
 def sort_members_by_name_length(list_of_members: list):
-    list_of_members.sort(key=lambda member: member['age'])
-    return sorted(list_of_members, key=lambda member: len(member['name']))
+    return sorted(list_of_members, key=lambda x: (len(x['name']), x['age']))
 
 
