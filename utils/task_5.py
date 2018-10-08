31413c002b7b6184a8ecacec045759585daa8c52
Igor Tagintsev <31354762+IgorTagintsev@users.noreply.github.com>
2018-10-08 20:05:25 +0300

    Fixed final bug in taks_5

--- utils/task_5.py
+++ utils/task_5.py
@@ -2,7 +2,7 @@ from functools import reduce
 
 def age(data: list):
     summ = reduce(lambda x, y: x + y, [member['age'] for member in data])
-    young = min(data, key=lambda x: list(x.values())[0])
-    old = max(data, key=lambda x: list(x.values())[0])
+    young = min(data, key=lambda x: x['age'])
+    old = max(data, key=lambda x: x['age'])
     return summ, young, old
 
